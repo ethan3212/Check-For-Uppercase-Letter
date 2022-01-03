17fa05ea0527883de49d7a0adc6369b955bd245f
ethan3212 <74456094+ethan3212@users.noreply.github.com>
2022-01-03 08:36:44 -0600

    Add files via upload

---
+++ Checking Uppercase Letters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Checking_Uppercase_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            string inputString;
+            const char QUIT = '!';
+            Write("Enter an uppercase letter: ");
+            inputString = ReadLine();
+            letter = Convert.ToChar(inputString);
+
+            while(letter != '!')
+            {
+                if (letter >= 'A' && letter <= 'Z')
+                    WriteLine("OK");
+                else
+                    WriteLine("Sorry -- that was not an uppercase letter");
+                WriteLine("Enter an uppercase letter or {0} to quite: ", QUIT);
+                inputString = ReadLine();
+                letter = Convert.ToChar(inputString);
+
+            }
+        }
+    }
+}
